b66dcb58687e281726e70606a0512c423a2667d9
Mertcan Yücel <root@T1MB97524.local>
2020-04-18 22:38:39 +0300

    update on config and site info

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,11 +7,11 @@
 module.exports = {
   /* Your site config here */
   siteMetadata: {
-    title: `Delog`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://delog-w3layouts.netlify.com/`,
+    title: `Mertcan Yücel`,
+    description: `Mertcan Yücel's Personal Website`,
+    siteUrl: `https://mertcanyucel.com`,
     home: {
-      title: `Hi! I'm Delog`,
+      title: `Hi! I'm Mertcan`,
       description: `I have been specifically designed to become a digital home for designers and developers, help them build amazing professional looking websites with ease. You don't have to worry about nitty gritty of web hosting services to run a blog and yet take full advantage of CMS to manage content :)`,
     },
     /* W3Layouts domain verification key for contact forms https://my.w3layouts.com/Forms/ */
